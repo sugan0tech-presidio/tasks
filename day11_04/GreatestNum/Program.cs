1d840c370d3a26187578f87895f860d1cebe0721
sugan0tech-presidio <sugavaneshmurugesan@gmail.com>
2024-04-11 14:51:11 +0530

    phase2 of task2 avg of nums dividable by 7

--- day11_04/GreatestNum/Program.cs
+++ day11_04/GreatestNum/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Enter Values, give negative to end & get the result");
             double max = 0;
+            double avgDivBySeven = 0;
+            int avgDivBySevenCount = 0;
             while (true)
             {
                 Console.WriteLine("val:");
@@ -14,9 +16,15 @@
                 {
                     break;
                 }
+                if(input%7 == 0)
+                {
+                    avgDivBySeven += input;
+                    avgDivBySevenCount++;
+                }
                 max = input > max ? input : max;
             }
             Console.WriteLine($"The Greatest Value of All Time is : {max}");
+            Console.WriteLine($"The avg of nums divsible by 7 is : {avgDivBySeven / avgDivBySevenCount}");
         }
     }
 }
