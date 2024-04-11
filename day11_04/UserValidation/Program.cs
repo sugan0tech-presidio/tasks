c15cde5046fb6a97c980101a80d3a15d95777704
sugan0tech-presidio <sugavaneshmurugesan@gmail.com>
2024-04-11 16:57:25 +0530

    task7 ordering words by least repeated vowels alone

--- day11_04/UserValidation/Program.cs
+++ day11_04/UserValidation/Program.cs
@@ -4,31 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the name:");
-            string name = Console.ReadLine()??"";
-            Console.WriteLine($"{name} length is {getSize(name)}");
-
-            int maxTry = 3;
-            while(maxTry > 0 )
+            Console.WriteLine("Enter 1 for sample user Auth, 2 for least vowels frequence problem");
+            var options = Console.ReadLine();
+            if (options == "1")
             {
                 Console.WriteLine("Enter the name:");
-                name = Console.ReadLine() ?? "";
-                Console.WriteLine("Enter the password:");
-                string password = Console.ReadLine()??"";
-                if (auth(name, password))
-                {
-                    Console.WriteLine("Auth successful");
-                    Console.WriteLine($"Welcome {name}!!");
-                    return;
-                }
-                else
-                {
-                    maxTry--;
-                    Console.WriteLine($"You only have {maxTry} attempts left.");
-                }
+                string name = Console.ReadLine() ?? "";
+                Console.WriteLine($"{name} length is {getSize(name)}");
 
+                int maxTry = 3;
+                while (maxTry > 0)
+                {
+                    Console.WriteLine("Enter the name:");
+                    name = Console.ReadLine() ?? "";
+                    Console.WriteLine("Enter the password:");
+                    string password = Console.ReadLine() ?? "";
+                    if (auth(name, password))
+                    {
+                        Console.WriteLine("Auth successful");
+                        Console.WriteLine($"Welcome {name}!!");
+                        return;
+                    }
+                    else
+                    {
+                        maxTry--;
+                        Console.WriteLine($"You only have {maxTry} attempts left.");
+                    }
+
+                }
+                Console.WriteLine("You are out of try!!!, Try after sometime.");
             }
-            Console.WriteLine("You are out of try!!!, Try after sometime.");
+            else if( options == "2")
+            {
+                var vowels = new Vowels();
+                Console.WriteLine("Enter the comma seperated string for words with least vowels");
+                var str = Console.ReadLine();
+                var res = vowels.getLeastWords(str ?? "");
+                foreach (var item in res.Keys)
+                {
+                    Console.WriteLine(item);
+                    Console.WriteLine($"count {res[item]}");
+                }
+            }
+            Console.WriteLine("Thank you!!!");
+
         }
 
         static bool auth(string username, string password)
---
+++ day11_04/UserValidation/Vowels.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserValidation
+{
+    internal class Vowels
+    {
+        HashSet<Char> vowels = new HashSet<Char>() { 'a', 'e', 'i', 'o', 'u'};
+
+        /*
+         * for getting Frequency map
+         */
+        private Dictionary<Char, int> getFrequency(string str)
+        {
+            Dictionary<Char, int> map = new Dictionary<Char, int>();
+            foreach (var ch in str.ToCharArray())
+            {
+                if (vowels.Contains(ch))
+                {
+                   map[ch] = map.GetValueOrDefault(ch, 0) + 1;
+                }
+            }
+            return map;
+        }
+
+        private int getVowelsCount(string str)
+        {
+            int count = 0;
+            foreach(var ch in str.ToCharArray())
+            {
+                if (vowels.Contains(ch))
+                    count++;
+            }
+            return count;
+        }
+
+        public Dictionary<string, int> getLeastWords(string input)
+        {
+            var inputList = input.Split(",").ToList();
+            var leastCount = getFreqNum(inputList[0]);
+            var res = new Dictionary<string, int>();
+
+            foreach (var word in inputList) {
+                var currCount = getFreqNum(word);
+                if( leastCount > currCount)
+                {
+                    leastCount = currCount;
+                    res.Clear();
+                    res[word] = getVowelsCount(word);
+                }
+            }
+            return res;
+
+        }
+        /*
+         * Returns a specialised comparitor num,
+         * for the vowels aaeiio it will be generated as 1122
+         * as frequencies in sorted order
+         */
+        private int getFreqNum(string word)
+        {
+            var wordFreq = getFrequency(word);
+            List<int> freqStore = new List<int>();
+            foreach (var item in wordFreq.Values)
+            {
+                freqStore.Add(item);
+            };
+            freqStore.Sort();
+
+            string compNo = "";
+
+            foreach (var item in freqStore)
+            {
+                compNo += item;
+            }
+            return int.Parse(compNo);
+        }
+
+    }
+}
