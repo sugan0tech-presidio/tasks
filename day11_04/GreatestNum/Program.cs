865d0b7a5b851ec2c03ef8838c9fa663a7ecb6f7
sugan0tech-presidio <sugavaneshmurugesan@gmail.com>
2024-04-11 14:45:11 +0530

    phase1 task2, Greatest Of All Numbers

---
+++ day11_04/GreatestNum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GreatestNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Values, give negative to end & get the result");
+            double max = 0;
+            while (true)
+            {
+                Console.WriteLine("val:");
+                double input = double.Parse(Console.ReadLine()??"0");
+                if(input <= 0)
+                {
+                    break;
+                }
+                max = input > max ? input : max;
+            }
+            Console.WriteLine($"The Greatest Value of All Time is : {max}");
+        }
+    }
+}
