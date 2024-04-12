2c6571940e1e4f8f539d2b518f3c07eb90213a62
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-04-12 12:38:15 +0530

    array update

--- day12_04/ModelTestSolution/Program.cs
+++ day12_04/ModelTestSolution/Program.cs
@@ -17,5 +17,38 @@ class Program
         
         var employee2 = new Employee(23, 2783.2, "suga", new DateTime(2001, 02, 20), "sie@mgia.com");
         employee2.PrintEmployeeDetails();
+
+
+        var employees = new Employee[2];
+        var program = new Program();
+        for (int i = 0; i < employees.Length; i++)
+            employees[i] = program.CreateEmployeeFromInput(i);
+
+        foreach (var emp in employees)
+            emp.PrintEmployeeDetails();
+
+    }
+
+    /// <summary>
+    /// Takes employee info like Name, DOB, Salary & Email and returns Employee object
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>fully populated Employee object</returns>
+    Employee CreateEmployeeFromInput(int id)
+    {
+        var employee = new Employee(id);
+        Console.WriteLine("Please enter the employee name");
+        employee.Name = Console.ReadLine();
+        Console.WriteLine("Please enter the employee Date of Birth in the format of yyyy/mm/dd");
+        employee.DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+        Console.WriteLine("Please enter the employee salary");
+        double salary;
+        while (!double.TryParse(Console.ReadLine(), out salary))
+            Console.WriteLine("Invalid entry");
+        employee.Salary = salary;
+        Console.WriteLine("Please enter the employee mail");
+        employee.Email = Console.ReadLine();
+        
+        return employee;
     }
 }
