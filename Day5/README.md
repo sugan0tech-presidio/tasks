4803af3f3c4b685c7e162fb0192cbed68c3a5245
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-04-15 10:28:17 +0530

    conditionalss

---
+++ Day5/README.md
@@ -0,0 +1,12 @@
+# Day5 Mon 15 Apr 2024
+
+Overview
+
+Learning
+---
+ - Conditional statements
+ - switch statements
+
+Tasks
+---
+  - [x] Employee request application to simulate the use of conditional and switch statements
---
+++ Day5/RequestTrackerApplication/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace RequestTrackerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Request Tracker!");
+
+            Console.WriteLine("Enter the type of request (1: Leave, 2: Equipment, 3: Training):");
+            int requestType = int.Parse(Console.ReadLine());
+
+            var program = new Program();
+            program.ProcessRequest(requestType);
+
+            Console.WriteLine("Thanks for using employee tracker application!!!");
+        }
+
+        /// <summary>
+        /// batch request processing
+        /// </summary>
+        /// <param name="requestType"></param>
+         void ProcessRequest(int requestType)
+        {
+            switch (requestType)
+            {
+                case 1: // Leave
+                    Console.WriteLine("Leave request received.");
+                    HandleLeaveRequest();
+                    break;
+                case 2: // Equipment
+                    Console.WriteLine("Equipment request received.");
+                    HandleEquipmentRequest();
+                    break;
+                case 3: // Training
+                    Console.WriteLine("Training request received.");
+                    HandleTrainingRequest();
+                    break;
+                default:
+                    Console.WriteLine("Invalid request type.");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handeling leave request dummy
+        /// </summary>
+        void HandleLeaveRequest()
+        {
+                                                      
+            Console.WriteLine("Processing leave request...");
+            bool isLeaveApproved = true;
+                                           
+            if (isLeaveApproved)
+            {
+                Console.WriteLine("Leave request approved!");
+            }
+            else
+            {
+                Console.WriteLine("Leave request denied.");
+            }
+        }
+
+        /// <summary>
+        /// Equeipment resuest handler
+        /// </summary>
+        void HandleEquipmentRequest()
+        {
+                                                          
+            Console.WriteLine("Processing equipment request...");
+            bool isEquipmentProvided = true;
+                                  
+            if (isEquipmentProvided)
+            {
+                Console.WriteLine("Equipment provided successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Unable to provide the equipment.");
+            }
+        }
+
+        /// <summary>
+        /// Training request handler
+        /// </summary>
+        void HandleTrainingRequest()
+        {
+                                                         
+            Console.WriteLine("Processing training request...");
+            bool isTrainingScheduled = true;
+                                               
+            if (isTrainingScheduled)
+            {
+                Console.WriteLine("Training scheduled successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Unable to schedule the training.");
+            }
+        }
+    }
+}
