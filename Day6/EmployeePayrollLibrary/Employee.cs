3c76108d877ef6cbba200c8be4815dc59d37dfd6
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-04-16 15:46:27 +0530

    phase 1: added abstract class

---
+++ Day6/EmployeePayrollLibrary/Employee.cs
@@ -0,0 +1,44 @@
+﻿namespace EmployeePayrollLibrary;
+
+public abstract class Employee: GovtRules
+{
+    public Employee()
+    {
+        EmpId = 0;
+        Name = string.Empty;
+    }
+
+    public Employee(int empId, string name)
+    {
+        EmpId = empId;
+        Name = name;
+    }
+
+    public int EmpId { get; set; }
+    public string Name { get; set; }
+    public int DepartmentId { get; set; }
+    public double BasicSalary { get; set; }
+
+    /// <summary>
+    ///     Gets input from user and updates directly.
+    /// </summary>
+    public virtual void BuildEmployeeFromConsole()
+    {
+        Console.Write("Please enter the Name\t:");
+        Name = Console.ReadLine() ?? string.Empty;
+        Console.Write("Please enter the Department Id\t:");
+    }
+    
+    public override string ToString()
+    {
+        return $"Employee {EmpId} Details:\n"
+               +$"\tEmployee type\t:\t{GetType()}\n"
+               +$"\tEmployee Name\t:\t{Name}\n"
+               +$"\tDepartment\t:\t{DepartmentId}\n"
+               +$"\tEmployee Age\t:\t{BasicSalary}\n";
+    }
+
+    public abstract double EmployeePF(double basicSalary);
+    public abstract string LeaveDetails();
+    public abstract double gratuityAmount(float serviceCompleted, double basicSalary);
+}
---
+++ Day6/EmployeePayrollLibrary/GovtRules.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeePayrollLibrary;
+
+public interface GovtRules
+{
+    public double EmployeePF(double basicSalary);
+    public string LeaveDetails();
+    public double gratuityAmount(float serviceCompleted, double basicSalary);
+}
