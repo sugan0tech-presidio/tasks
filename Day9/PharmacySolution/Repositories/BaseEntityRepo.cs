e5f76ff0b80ce36faea90fb5be374c986f13e2e9
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-04-22 10:52:32 +0530

    Add: Base repos

---
+++ Day9/PharmacySolution/Repositories/BaseEntityRepo.cs
@@ -0,0 +1,58 @@
+﻿using PharmacyModels;
+
+namespace PharmacyManagement.Repositories;
+
+public abstract class BaseEntityRepo : IBaseRepo<BaseEntity>
+{
+    private readonly Dictionary<int, BaseEntity> _entities = new ();
+
+    public BaseEntity GetById(int id)
+    {
+        if (_entities.TryGetValue(id, out var entity))
+            throw new KeyNotFoundException($"{GetType()} with key {id} not found!!!");
+        
+        return entity;
+    }
+
+    public List<BaseEntity> GetAll()
+    {
+        return _entities.Values.ToList();
+    }
+
+    public BaseEntity Add(BaseEntity entity)
+    {
+        if (entity == null)
+            throw new ArgumentNullException(nameof(entity), $"{GetType()} cannot be null.");
+
+        var currSeq = 1;
+        while (_entities.ContainsKey(currSeq))
+            currSeq++;
+        entity.Id = currSeq;
+        
+        _entities.Add(currSeq, entity);
+        return entity;
+    }
+
+    public BaseEntity Update(BaseEntity entity)
+    {
+        var id = entity.Id;
+        if (_entities.ContainsKey(id))
+        {
+            _entities[id] = entity;
+        }
+        else
+        {
+            throw new KeyNotFoundException("Entity not found.");
+        }
+
+        return _entities[id];
+    }
+
+    public void Delete(int id)
+    {
+        if (!_entities.Remove(id))
+        {
+            throw new KeyNotFoundException("Entity not found.");
+        }
+    }
+}
---
+++ Day9/PharmacySolution/Repositories/IBaseRepo.cs
@@ -0,0 +1,10 @@
+﻿namespace PharmacyManagement.Repositories;
+
+public interface IBaseRepo<TEntity>
+{
+    TEntity GetById(int id);
+    List<TEntity> GetAll();
+    TEntity Add(TEntity entity);
+    TEntity Update(TEntity entity);
+    void Delete(int id);
+}
