b3506e41bf8c8657089bcbee52a93c5e99644f33
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-04-23 17:08:28 +0530

    working dependent test

---
+++ Others/DoctorAppointmentManager.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using DoctorAppointmentManager.Entities;
+
+namespace DoctorAppointmentManager.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
