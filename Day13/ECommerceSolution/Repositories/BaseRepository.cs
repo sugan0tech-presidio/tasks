b4ed723520437ec039d49d3a254bb5f0282fd2fa
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-04-29 14:16:35 +0530

    add: summary comments for base

--- Day13/ECommerceSolution/Repositories/BaseRepository.cs
+++ Day13/ECommerceSolution/Repositories/BaseRepository.cs
@@ -1,72 +1,101 @@
 ﻿using ECommerceApp.Entities;
 
-namespace ECommerceApp.Repositories
+namespace ECommerceApp.Repositories;
+
+/// <summary>
+/// A base repository providing basic CRUD operations for entities implementing the IEntity interface.
+/// </summary>
+/// <typeparam name="TBaseEntity">The type of entity.</typeparam>
+public abstract class BaseRepository<TBaseEntity> : IBaseRepository<TBaseEntity> where TBaseEntity : IEntity
 {
-    public abstract class BaseRepository<TBaseEntity> : IBaseRepository<TBaseEntity> where TBaseEntity : IEntity
+    protected readonly List<TBaseEntity> Entities = new ();
+    private readonly object _lock = new ();
+
+    /// <summary>
+    /// Retrieves an entity by its unique identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to retrieve.</param>
+    /// <returns>The entity with the specified identifier.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if the entity with the specified identifier is not found.</exception>
+    public async Task<TBaseEntity> GetByIdAsync(int id)
     {
-        protected readonly List<TBaseEntity> Entities = new List<TBaseEntity>();
-        private readonly object _lock = new object();
-
-        public async Task<TBaseEntity> GetByIdAsync(int id)
+        lock (_lock)
         {
-            lock (_lock)
-            {
-                var entity = Entities.FirstOrDefault(e => e.Id == id);
-                if (entity == null)
-                    throw new KeyNotFoundException($"{GetType()} with key {id} not found!!!");
-                return entity;
-            }
-        }
-
-        public async Task<List<TBaseEntity>> GetAllAsync()
-        {
-            lock (_lock)
-            {
-                Entities.Sort();
-                return Entities.ToList();
-            }
-        }
-
-        public async Task<TBaseEntity> AddAsync(TBaseEntity entity)
-        {
+            var entity = Entities.FirstOrDefault(e => e.Id == id);
             if (entity == null)
-                throw new ArgumentNullException(nameof(entity), $"{GetType()} cannot be null.");
-
-            lock (_lock)
-            {
-                var currSeq = 1;
-                while (Entities.Any(e => e.Id == currSeq))
-                    currSeq++;
-                entity.Id = currSeq;
-
-                Entities.Add(entity);
-                return entity;
-            }
+                throw new KeyNotFoundException($"{typeof(TBaseEntity)} with key {id} not found!!!");
+            return entity;
         }
+    }
 
-        public async Task<TBaseEntity> UpdateAsync(TBaseEntity entity)
+    /// <summary>
+    /// Retrieves all entities asynchronously.
+    /// </summary>
+    /// <returns>A list of all entities.</returns>
+    public async Task<List<TBaseEntity>> GetAllAsync()
+    {
+        lock (_lock)
         {
-            lock (_lock)
-            {
-                var existingEntity = GetByIdAsync(entity.Id).Result;
-                if (existingEntity != null)
-                {
-                    var index = Entities.IndexOf(existingEntity);
-                    Entities[index] = entity;
-                }
-                return entity;
-            }
+            Entities.Sort();
+            return Entities.ToList();
         }
+    }
 
-        public async Task DeleteAsync(int id)
+    /// <summary>
+    /// Adds a new entity asynchronously.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
+    /// <returns>The added entity.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided entity is null.</exception>
+    public async Task<TBaseEntity> AddAsync(TBaseEntity entity)
+    {
+        if (entity == null)
+            throw new ArgumentNullException(nameof(entity), $"{typeof(TBaseEntity)} cannot be null.");
+
+        lock (_lock)
         {
-            lock (_lock)
+            var currSeq = 1;
+            while (Entities.Any(e => e.Id == currSeq))
+                currSeq++;
+            entity.Id = currSeq;
+
+            Entities.Add(entity);
+            return entity;
+        }
+    }
+
+    /// <summary>
+    /// Updates an existing entity asynchronously.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
+    /// <returns>The updated entity.</returns>
+    public async Task<TBaseEntity> UpdateAsync(TBaseEntity entity)
+    {
+        lock (_lock)
+        {
+            var existingEntity = GetByIdAsync(entity.Id).Result;
+            if (existingEntity != null)
             {
-                var entityToRemove = GetByIdAsync(id).Result;
-                if (entityToRemove != null)
-                {
-                    Entities.Remove(entityToRemove);
-                }
+                var index = Entities.IndexOf(existingEntity);
+                Entities[index] = entity;
+            }
+
+            return entity;
+        }
+    }
+
+    /// <summary>
+    /// Deletes an entity by its unique identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to delete.</param>
+    public async Task DeleteAsync(int id)
+    {
+        lock (_lock)
+        {
+            var entityToRemove = GetByIdAsync(id).Result;
+            if (entityToRemove != null)
+            {
+                Entities.Remove(entityToRemove);
             }
         }
     }
--- Day13/ECommerceSolution/Services/BaseService.cs
+++ Day13/ECommerceSolution/Services/BaseService.cs
@@ -1,72 +1,105 @@
 ﻿using ECommerceApp.Entities;
 using ECommerceApp.Repositories;
 
-namespace ECommerceApp.Services
+namespace ECommerceApp.Services;
+
+/// <summary>
+/// A base service implementation for entities.
+/// </summary>
+/// <typeparam name="TBaseEntity">The type of the entity.</typeparam>
+public abstract class BaseService<TBaseEntity> where TBaseEntity : IEntity
 {
+    protected readonly BaseRepository<TBaseEntity> Repository;
+
     /// <summary>
-    /// A base service implementation for entities.
+    /// Constructs a new instance of the BaseService class.
     /// </summary>
-    /// <typeparam name="TBaseEntity">The type of the entity.</typeparam>
-    public abstract class BaseService<TBaseEntity> where TBaseEntity : IEntity
+    /// <param name="repository">The repository instance to be used by the service.</param>
+    /// <exception cref="ArgumentNullException">Thrown if the provided repository is null.</exception>
+    protected BaseService(BaseRepository<TBaseEntity> repository)
     {
-        protected readonly BaseRepository<TBaseEntity> Repository;
+        Repository = repository ?? throw new ArgumentNullException(nameof(repository),
+            $"{typeof(BaseService<TBaseEntity>)} cannot be null.");
+    }
 
-        protected BaseService(BaseRepository<TBaseEntity> repository)
+    /// <summary>
+    /// Retrieves an entity by its unique identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to retrieve.</param>
+    /// <returns>The entity with the specified identifier.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if the entity with the specified identifier is not found.</exception>
+    public async Task<TBaseEntity> GetByIdAsync(int id)
+    {
+        try
         {
-            Repository = repository ?? throw new ArgumentNullException(nameof(repository), $"{GetType()} cannot be null.");
+            return await Repository.GetByIdAsync(id);
         }
-
-        public async Task<TBaseEntity> GetByIdAsync(int id)
+        catch (KeyNotFoundException ex)
         {
-            try
-            {
-                return await Repository.GetByIdAsync(id);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                throw new KeyNotFoundException($"Failed to retrieve {typeof(TBaseEntity).Name} with ID {id}.", ex);
-            }
+            throw new KeyNotFoundException($"Failed to retrieve {typeof(TBaseEntity).Name} with ID {id}.", ex);
         }
+    }
 
-        public async Task<List<TBaseEntity>> GetAllAsync()
+    /// <summary>
+    /// Retrieves all entities asynchronously.
+    /// </summary>
+    /// <returns>A list of all entities.</returns>
+    public async Task<List<TBaseEntity>> GetAllAsync()
+    {
+        return await Repository.GetAllAsync();
+    }
+
+    /// <summary>
+    /// Adds a new entity asynchronously.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
+    /// <returns>The added entity.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided entity is null.</exception>
+    public async Task<TBaseEntity> AddAsync(TBaseEntity entity)
+    {
+        try
         {
-            return await Repository.GetAllAsync();
+            return await Repository.AddAsync(entity);
         }
-
-        public async Task<TBaseEntity> AddAsync(TBaseEntity entity)
+        catch (ArgumentNullException)
         {
-            try
-            {
-                return await Repository.AddAsync(entity);
-            }
-            catch (ArgumentNullException)
-            {
-                throw;
-            }
+            throw;
         }
+    }
 
-        public async Task<TBaseEntity> UpdateAsync(TBaseEntity entity)
+    /// <summary>
+    /// Updates an existing entity asynchronously.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
+    /// <returns>The updated entity.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if the entity to update is not found.</exception>
+    public async Task<TBaseEntity> UpdateAsync(TBaseEntity entity)
+    {
+        try
         {
-            try
-            {
-                return await Repository.UpdateAsync(entity);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                throw new KeyNotFoundException($"Failed to update {typeof(TBaseEntity).Name}. Entity not found.", ex);
-            }
+            return await Repository.UpdateAsync(entity);
         }
-
-        public virtual async Task DeleteAsync(int id)
+        catch (KeyNotFoundException ex)
         {
-            try
-            {
-                await Repository.DeleteAsync(id);
-            }
-            catch (KeyNotFoundException ex)
-            {
-                throw new KeyNotFoundException($"Failed to delete {typeof(TBaseEntity).Name}. Entity not found.", ex);
-            }
+            throw new KeyNotFoundException($"Failed to update {typeof(TBaseEntity).Name}. Entity not found.", ex);
+        }
+    }
+
+    /// <summary>
+    /// Deletes an entity by its unique identifier asynchronously.
+    /// </summary>
+    /// <param name="id">The unique identifier of the entity to delete.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    /// <exception cref="KeyNotFoundException">Thrown if the entity to delete is not found.</exception>
+    public virtual async Task DeleteAsync(int id)
+    {
+        try
+        {
+            await Repository.DeleteAsync(id);
+        }
+        catch (KeyNotFoundException ex)
+        {
+            throw new KeyNotFoundException($"Failed to delete {typeof(TBaseEntity).Name}. Entity not found.", ex);
         }
     }
 }
