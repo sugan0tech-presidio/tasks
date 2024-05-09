a672ae9f102167d3b4f747775f6b379bd1878cf2
sugan0tech-presidio <sugavaneshmurugesan@presidio.com>
2024-05-09 22:41:32 +0530

    add: EF learning migration

---
+++ Day21/SuperHero/Entities/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero.Entities.Data
+{
+    internal class DataContext: DbContext
+    {
+        // should be name of the table
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=7TXBBX3\SQLEXPRESS;Integrated Security=true;Initial Catalog=SuperHero;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Day21/SuperHero/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
